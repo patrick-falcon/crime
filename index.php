62930c462358a859b9c3bc3c1057074bc9703e53
Patrick Falcon <patrickfalcon2807@gmail.com>
2018-06-25 00:21:37 +0100

    e

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ ini_set("allow_url_fopen",1);
 $json = file_get_contents("https://data.police.uk/api/crimes-street/all-crime?lat=51.5806&lng=0.3420&date=2018-02");
 $obj = json_decode($json);
 
-print_r(array_values($obj));
+print_r($obj["category"]);
 ?>
