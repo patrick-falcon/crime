9dfbfb0e353bba0d98e1059f6fc6d043ac28cb44
Patrick Falcon <patrickfalcon2807@gmail.com>
2018-06-25 00:25:08 +0100

    implement stylesheet

--- index.php
+++ index.php
@@ -5,5 +5,11 @@ ini_set("allow_url_fopen",1);
 $json = file_get_contents("https://data.police.uk/api/crimes-street/all-crime?lat=51.5806&lng=0.3420&date=2018-02");
 $obj = json_decode($json);
 
-print_r($obj["category"]);
+echo "<font color='red'>";
+
+print_r($obj);
+
+echo "</font>";
 ?>
+
+<link href="bootstrap.css" rel="stylesheet">
