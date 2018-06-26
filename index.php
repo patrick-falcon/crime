722241fc419c6f5e0789efa097eeccc7a9c78a98
Patrick Falcon <patrickfalcon2807@gmail.com>
2018-06-26 03:18:28 +0100

    .

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 
 ini_set("allow_url_fopen",1);
 
-$json = file_get_contents("https://data.police.uk/api/crimes-street/all-crime?lat=51.5806&lng=0.3420&date=2018-02");
+$json = file_get_contents("https://data.police.uk/api/crimes-street/all-crime?lat=51.5806&lng=0.3420&date=2018-03");
 $obj = json_decode($json);
 
 echo "<font color='red'>";
 
-print_r($obj);
+//print_r($obj);
+
+$x = implode($obj);
+
+if (strpos($x,'anti-social-behaviour') {
+  echo "Anti social behaviour"; 
+}
 
 echo "</font>";
 ?>
